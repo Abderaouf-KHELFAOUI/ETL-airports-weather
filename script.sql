261575d02bbee3b048c3b47f98df8a33bbdfed37
Raouf <khelfaoui.abderraouf@gmail.com>
2025-12-08 20:25:42 +0100

    modified tables and DBP notebook

--- script.sql
+++ script.sql
@@ -1,30 +1,30 @@
 
 CREATE TABLE AIRLINES (
     airline_code CHAR(2) PRIMARY KEY,
-    airline_name VARCHAR(100)
+    airline_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE AIRPORTS (
     iata_code CHAR(3) PRIMARY KEY,
     airport_name VARCHAR(100) NOT NULL,
-    city VARCHAR(50),
-    state CHAR(2),
-    latitude FLOAT,
-    longitude FLOAT,
+    city VARCHAR(50) NOT NULL,
+    state CHAR(2) NOT NULL,
+    latitude FLOAT ,
+    longitude FLOAT ,
     
     CHECK(latitude BETWEEN -90 AND 90 AND longitude BETWEEN -180 AND 180)
 );
 
 CREATE TABLE FLIGHTS (
     flight_id INTEGER PRIMARY KEY,
-    flight_date DATE,
-    flight_number VARCHAR(10),
-    dep_time TEXT,
-    dep_delay INTEGER,
-    cancelled BOOLEAN,
-    airline_code CHAR(2),
-    origin_airport CHAR(3),
-    dest_airport CHAR(3),
+    flight_date DATE NOT NULL,
+    flight_number VARCHAR(10) NOT NULL,
+    dep_time TEXT ,
+    dep_delay INTEGER NOT NULL,
+    cancelled BOOLEAN NOT NULL,
+    airline_code CHAR(2) NOT NULL,
+    origin_airport CHAR(3) NOT NULL,
+    dest_airport CHAR(3) NOT NULL,
     CHECK(dep_time GLOB '[0-2][0-9]:[0-5][0-9]:[0-5][0-9]')
     FOREIGN KEY (airline_code) 
         REFERENCES AIRLINES(airline_code),
@@ -36,9 +36,9 @@ CREATE TABLE FLIGHTS (
 
 CREATE TABLE WEATHER (
     weather_id INTEGER PRIMARY KEY,
-    reading_time TIMESTAMP,
+    reading_time TIMESTAMP NOT NULL,
     wind_speed FLOAT,
     temperature FLOAT,
-    airport_code CHAR(3),
+    airport_code CHAR(3) NOT NULL,
     FOREIGN KEY (airport_code) REFERENCES AIRPORTS(iata_code)
 );
